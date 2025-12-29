b0972156ae38fc1ac95720363034da7b8f0ba3d9
kateslay <kvin0304@yandex.ru>
2025-12-29 16:29:40 +0300

    удалила лишние файлы

--- function.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h> //ввод вывод
-#include <stdlib.h> //для выделения памяти с malloc, srand, rand
-#include <locale.h> //для русского языка
-#include <time.h> //для генерации случайной матрицы
